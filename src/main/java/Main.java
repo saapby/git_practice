9e653b522f979f6c5c5131f613e4886320b565ee
saapby <saap.net@gmail.com>
2023-01-20 02:27:14 +0300

    Updated General Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import zoo.MainZoo;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Zoo!");
+        MainZoo.main(args);
     }
 }
